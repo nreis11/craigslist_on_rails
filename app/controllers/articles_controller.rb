a95640f2aa3e70e98edd65059e7e57011d34edc0
reeses-pieces <nicholasreis@icloud.com>
2016-10-30 11:47:29 -0700

    add delete link to category show page and add destroy action to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,15 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    # TODO: IMPLEMENT UPDATE ACTION
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to category_path(@article.category)
   end
 
 
