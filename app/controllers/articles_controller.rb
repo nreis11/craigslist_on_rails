c722707cb6388366521c9785851d121df29cdff4
reeses-pieces <nicholasreis@icloud.com>
2016-10-29 19:24:09 -0700

    add new article path and controller action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,31 @@
 class ArticlesController < ApplicationController
 
+  def new
+    @article = Article.new
+  end
+
   def show
     @article = Article.find(params[:id])
   end
+
+  def create
+
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      @errors = @article.errors.full_messages 
+      render :new
+    end
+  end
+
+
+  private
+
+  def article_params
+    params[:article][:price]
+    params.require(:article).permit(:title, :body, :price, :category_id)
+  end
+
 end
